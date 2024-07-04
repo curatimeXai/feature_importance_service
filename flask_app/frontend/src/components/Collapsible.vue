50af5201cae6b04e15849abe85f4f04d7d78868a
Alex <scristea@students.uni-mainz.de>
2024-07-04 22:05:59 +0200

    frequent questions

--- flask_app/frontend/src/components/Collapsible.vue
+++ flask_app/frontend/src/components/Collapsible.vue
@@ -11,7 +11,7 @@ const collapsed=ref(true)
       <i v-if="collapsed" class="fa fa-chevron-right" style="color: var(--primary-color)"></i>
       <i v-else class="fa fa-chevron-up" style="color: var(--primary-color)"></i>
     </div>
-    <div v-if="!collapsed" style="border-top: 1px solid var(--primary-color)">
+    <div v-if="!collapsed" style="border-top: 1px solid var(--primary-color); padding-top: 10px">
       <slot name="collapsible"></slot>
     </div>
   </div>
--- flask_app/frontend/src/components/Settings/FrequentQuestions.vue
+++ flask_app/frontend/src/components/Settings/FrequentQuestions.vue
@@ -8,14 +8,34 @@ import Collapsible from "@/components/Collapsible.vue";
     <div class="mb-1">
       Frequent Questions
     </div>
-    <Collapsible>
-      <template v-slot:title>
-        What does Bogalusa stand for?
-      </template>
-      <template v-slot:collapsible>
-        Bogalusa is a village in the USA, where a famous heart disease study took place from 1970 to 2010.
-      </template>machine learning system designed to improve the accuracy and fluency of Google Translate
-    </Collapsible>
+    <div class="row space-between">
+      <Collapsible class="col-6">
+        <template v-slot:title>
+          What does Bogalusa stand for?
+        </template>
+        <template v-slot:collapsible>
+          Bogalusa is a village in the USA, where a famous heart disease study took place from 1973 until today.
+        </template>
+      </Collapsible>
+      <Collapsible class="col-6">
+        <template v-slot:title>
+          What is this dashboard for?
+        </template>
+        <template v-slot:collapsible>
+          This dashboard is ment for workers in the medical field (especially cardiologists) to get acquainted with
+          AI-tools and help build trust in AI-models .
+        </template>
+      </Collapsible>
+       <Collapsible class="col-6">
+        <template v-slot:title>
+          Why is this dashboad only for heart disease prediction?
+        </template>
+        <template v-slot:collapsible>
+          One of  the main death causes are related to heart problems and this dashboard is ment to help doctors
+          diagnose patients better and see heart disease risks better.
+        </template>
+      </Collapsible>
+    </div>
   </div>
 </template>
 
